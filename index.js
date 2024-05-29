609d9acd4af24fbb44e4fd7fc60c2f3d0797af1a
Inder12801 <inder12801@gmail.com>
2024-05-29 15:23:52 +0530

    uppdated args and exec paths

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express, { json } from "express";
 import puppeteer from "puppeteer";
 import JSZip from "jszip";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = 5000;
@@ -22,7 +25,18 @@ app.post("/screenshot", async (req, res) => {
 
     console.log("url recieved : ", url);
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
+    });
     const page = await browser.newPage();
 
     const devices = [
